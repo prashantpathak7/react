277a507d5e87856396cea54e56e898257b7908a0
Prashant Pathak <prashant209997@gmail.com>
2022-01-15 14:12:10 +0530

    Updates

---
+++ precache-manifest.f4657644572a68f624645a1dcdc0b090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462920906e710f5fa4d1ff517453eb40",
+    "url": "/react-beginning/index.html"
+  },
+  {
+    "revision": "875a7581f3aeb4cbf2d9",
+    "url": "/react-beginning/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1b2ba433eff69518d8fe",
+    "url": "/react-beginning/static/js/2.a6eab94a.chunk.js"
+  },
+  {
+    "revision": "1ce896dddecdb4d51806",
+    "url": "/react-beginning/static/js/3.c541d426.chunk.js"
+  },
+  {
+    "revision": "875a7581f3aeb4cbf2d9",
+    "url": "/react-beginning/static/js/main.bdc3d529.chunk.js"
+  },
+  {
+    "revision": "58fa8c60e1ba03535bd4",
+    "url": "/react-beginning/static/js/runtime~main.6c06e780.js"
+  }
+]);
